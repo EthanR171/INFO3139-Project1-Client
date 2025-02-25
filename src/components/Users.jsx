711f8bea92e50ad0b281f61383a86fbfa2348b18
Ethan Rivers <143448114+EthanR171@users.noreply.github.com>
2025-02-25 12:03:40 -0500

    useEffect to only load users once
    *IMPORTANT*
    
    In development we will see the list in our console output two times because of <StrictMode> in App.jsx.

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,22 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Paper, CardHeader, CardContent } from '@mui/material';
 
 const Users = (props) => {
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = async () => {
+    let response = await fetch(`http://localhost:9000/api/users`);
+    let result = await response.json();
+    console.log(result);
+    setUsers(result);
+    return users;
+  };
+
+  // Runs once per rendering
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
   return (
     <>
       <Paper elevation={4} sx={{ marginTop: '1em' }}>
