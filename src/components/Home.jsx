122fd86f5e06222858fef526706cb284d90a3f02
Ethan Rivers <143448114+EthanR171@users.noreply.github.com>
2025-02-25 08:08:18 -0500

    Start Lab 11
    Added Comments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -93,8 +93,14 @@ const Home = () => {
   return (
     <>
       <AppBar position="sticky">
+        {/* Justify is in the same direction as flex-direction */}
+        {/* By default, it's row, so it's left-right */}
+        {/* The display: flex is required to control the children elements */}
+        {/* And space-between spaces them out */}
         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          {/* First element, to the left */}
           <Typography variant="h6">INFO-3139 - Project 1</Typography>
+          {/* Last element, to the left */}
           <IconButton color="inherit" onClick={handleMenuOpen}>
             <MenuIcon />
           </IconButton>
