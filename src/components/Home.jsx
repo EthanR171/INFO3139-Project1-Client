9daf4f01b7224c7efc4ef19cbb8b55cda2cdcb42
Ethan Rivers <143448114+EthanR171@users.noreply.github.com>
2025-03-02 01:38:53 -0500

    Added Auto complete component to list countries

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { Alert, Paper, CardHeader, CardContent, TextField, Button, IconButton, Menu, MenuItem } from '@mui/material';
+import { Alert, Paper, CardHeader, CardContent, TextField, Button, IconButton, Menu, MenuItem, Autocomplete, Box } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 import { useState, useEffect } from 'react';
@@ -7,10 +7,29 @@ import * as api from '../util/api.js';
 import logo from '../assets/Back2TheFuture.jpg';
 
 const Home = (props) => {
+  const [alerts, setAlerts] = useState([]); // State variable to hold Autocomplete options
+
   const loadAlerts = async () => {
     let result = await api.alerts.getSearchData();
-    console.log(result); // just printing to the console for now
-    props.alert(`${result.length} alerts loaded`); // feedback with the Snackbar
+    setAlerts(result);
+    props.alert(`${result.length} alerts loaded`);
+  };
+
+  // A simple transformation arrow function to "stringify" our alert
+  const alertAsText = (alert) => `(${alert.country_code}) ${alert.country_name}`;
+
+  // Function that will render each object as an text option in the Autocomplete
+  const renderAutocompleteOption = (props, option) => {
+    // Propagating props from the Autocomplete parent object
+    // Using the rest operator to "pull out" a property (key, in this case)
+    // key must be passed explicitly added and not within the ...props spread
+    const { key, ...nonKeyProps } = props;
+
+    return (
+      <Box key={key} {...nonKeyProps}>
+        <>{alertAsText(option)}</>
+      </Box>
+    );
   };
 
   useEffect(() => {
@@ -22,7 +41,17 @@ const Home = (props) => {
       <Paper elevation={4} sx={{ marginTop: '0.5em' }}>
         <CardHeader title="Travel Alerts" />
         <CardContent>
-          <img style={{ width: '30%', maxWidth: '200px' }} src={logo} />
+          <img style={{ width: '40%', maxWidth: '200px' }} src={logo} />
+          <Autocomplete
+            options={alerts} // array of objects [ { country_code: "", country_name: "" }, ... ]
+            autoHighlight
+            getOptionKey={(option) => option.country_code} // Needed in addition to the <Box key={key}>
+            getOptionLabel={(option) => alertAsText(option)} // Needs in addition to the renderAutocompleteOption
+            renderOption={renderAutocompleteOption} // Instructing how the data should be displayed
+            renderInput={(params) => <TextField {...params} label="Find Alert" />} // Controls the "search field" part
+            sx={{ marginTop: '0.5em' }}
+            onChange={(_event, selectedOption) => console.log(selectedOption)} // It's the 2nd argument that gives what's selected
+          />
         </CardContent>
       </Paper>
     </>
