cb3e2476c718043938481ca6d7e0a16264d557a2
Ethan Rivers <143448114+EthanR171@users.noreply.github.com>
2025-03-01 23:08:12 -0500

    Changed Color theme of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Snackbar } from '@mui/material';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { teal } from '@mui/material/colors';
 
 import Header from './components/Header.jsx';
 import Home from './components/Home.jsx';
@@ -9,6 +11,12 @@ import Users from './components/Users.jsx';
 import './App.css';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: { main: teal[500] },
+    },
+  });
+
   // Global state for snackbar.
   const [snackbar, setSnackbar] = useState({
     open: false,
@@ -40,21 +48,23 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Header alert={alert} refreshDatabase={refreshDatabase} />
-        <Routes>
-          <Route path="/" element={<Home alert={alert} />} />
-          <Route path="/users" element={<Users alert={alert} />} />
-        </Routes>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header alert={alert} refreshDatabase={refreshDatabase} />
+          <Routes>
+            <Route path="/" element={<Home alert={alert} />} />
+            <Route path="/users" element={<Users alert={alert} />} />
+          </Routes>
 
-        <Snackbar
-          open={snackbar.open}
-          autoHideDuration={5000}
-          // Clear the snackbar state on close
-          onClose={() => setSnackbar({ open: false, message: '' })}
-          message={snackbar.message}
-        />
-      </BrowserRouter>
+          <Snackbar
+            open={snackbar.open}
+            autoHideDuration={5000}
+            // Clear the snackbar state on close
+            onClose={() => setSnackbar({ open: false, message: '' })}
+            message={snackbar.message}
+          />
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
